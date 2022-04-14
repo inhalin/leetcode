1ca9cd59ba6e8dcb85c7f6b9b3569cfbde043866
Kimin <71866731+inhalin@users.noreply.github.com>
2022-04-14 22:17:06 +0900

    Time: 9 ms (84.76%), Space: 41.8 MB (85.06%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,27 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x == 0) return true;
-        int len = (int) Math.log10(x) + 1;
-        if (x < 0) len++;
-        int[] nums = new int[len];
-        int i = 0;
-        while (len > 0) {
-            nums[i++] = x % 10;
-            x = x / 10;
-            len--;
+        int reversed = 0;
+        int cloned = x;
+        
+        while (cloned > 0) {
+            reversed = reversed * 10 + cloned % 10;
+            cloned /= 10;
         }
         
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            if (nums[left] == nums[right]) {
-                left++;
-                right--;
-            } else {
-                return false;
-            }
-        }
-        
-        return true;
+        return x == reversed;
     }
 }
