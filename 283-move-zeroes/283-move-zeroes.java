98f524062c8c48fa651d2d6a8747fa9a0b052c4d
Kimin <inhalin.kim@gmail.com>
2022-08-09 10:05:58 +0900

    Time: 2 ms (82.89%), Space: 54.8 MB (44.83%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        
+        while (i < nums.length) {
+            if (nums[i] == 0) {
+                i++;
+            } else {
+                nums[j] = nums[i];
+                i++;
+                j++;
+            }
+        }
+        
+        while (j < nums.length) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+}
