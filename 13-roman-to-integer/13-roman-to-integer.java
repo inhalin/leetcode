81f503c16bbffdb480a50cb4bbbd702ab7e9ce35
Kimin <inhalin.kim@gmail.com>
2022-08-16 22:27:12 +0900

    Time: 13 ms (31.15%), Space: 47 MB (26.99%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,21 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> roman = new HashMap<>();
-        roman.put("I", 1);
-        roman.put("V", 5);
-        roman.put("X", 10);
-        roman.put("L", 50);
-        roman.put("C", 100);
-        roman.put("D", 500);
-        roman.put("M", 1000);
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
 
-        String[] split = s.split("");
-        int sum = roman.get(split[split.length - 1]);
-        for (int i = split.length - 1; i > 0; i--) {
-            if (roman.get(split[i]) <= roman.get(split[i - 1])) {
-                sum += roman.get(split[i - 1]);
+        int sum = roman.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 1; i > 0; i--) {
+            if (roman.get(s.charAt(i)) <= roman.get(s.charAt(i - 1))) {
+                sum += roman.get(s.charAt(i - 1));
             } else {
-                sum -= roman.get(split[i - 1]);
+                sum -= roman.get(s.charAt(i - 1));
             }
         }
         return sum;
